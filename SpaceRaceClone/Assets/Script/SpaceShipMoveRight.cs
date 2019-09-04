0b0c8b1b898f3580305d81431a0df2de79051479
Ren H <kerensahughes@gmail.com>
2019-09-04 16:07:14 -0400

    Basic Movement Finished
    9/4/2019

---
+++ SpaceRaceClone/Assets/Script/SpaceShipMoveRight.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//INTENT: Move the Right player's Spaceship up and down
+//USAGE: Place on Right PC
+public class SpaceShipMoveRight : MonoBehaviour {
+    
+    private Vector3 StartPos; //declaring StartPos V3
+	
+	
+
+    // Use this for initialization
+    void Start () {
+        StartPos = new Vector3(5.8f, -5.5f,0); //establish Start Pos
+    }
+	
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(KeyCode.Keypad8)) //use 8 on the number pad to...
+        {
+            transform.Translate(0,0.12f,0 ); //move up
+        }
+
+        if (Input.GetKey(KeyCode.Keypad2)) //use 2 on the number pad to..
+        {
+            transform.Translate(0,-0.12f,0); //move down
+            
+        }
+		
+
+        if (transform.position.y > 5.6f) //if the player leaves the game screen
+        {
+            transform.position = StartPos; //place them at the start position
+        }
+    }
+}
---
+++ SpaceRaceClone/Assets/Script/SpaceshipMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//INTENT: Move the Left player's Spaceship up and down
+//USAGE: Place on Left PC
+public class SpaceshipMove : MonoBehaviour
+{
+	private Vector3 StartPos; //declaring StartPos V3
+	
+	
+
+	// Use this for initialization
+	void Start () {
+		StartPos = new Vector3(-5.8f, -5.5f,0); //establish Start Pos
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W))//use W to...
+		{
+			transform.Translate(0,0.12f,0 ); //move up
+		}
+
+		if (Input.GetKey(KeyCode.S)) //use S to...
+		{
+			transform.Translate(0,-0.12f,0); //move down
+			
+		}
+		
+
+		if (transform.position.y > 5.6f) //if player leaves top of game screen
+		{
+			transform.position = StartPos; //put them back at Start pos
+		}
+	}
+}
