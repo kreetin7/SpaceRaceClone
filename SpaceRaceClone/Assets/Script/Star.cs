42efe1b55f676d469e664053b6dc26414c436f63
Ren H <kerensahughes@gmail.com>
2019-09-05 15:12:52 -0400

    Stars Move and reappear
    Stars move across the screen, reappear on the left side after going off screen. Also black background implemented.

---
+++ SpaceRaceClone/Assets/Script/Star.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+
+	public GameObject StarPrefab;
+	
+	 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0.01f,0,0);
+
+		if (transform.position.x > 12.5f)
+		{
+			transform.position = new Vector3(-12f, transform.position.y, 0);
+		}
+	}
+}
